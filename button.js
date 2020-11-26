198e5f4477fa68e11be3859c126a39c8fa18f21d
zhirobyte <57735877+zhirobyte@users.noreply.github.com>
2020-11-26 21:52:16 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,13 @@
+      var nilaia = 1;
+      var nilaib = 1;
+      var result = nilaia + nilaib;
+
+      if (function A() == true) {
+        window.alert(nilaia);
+      }
+      else if (function B() == true) {
+        window.alert(nilaib);
+      }
+      else if (function C() == true) {
+        window.alert(result);
+      }
